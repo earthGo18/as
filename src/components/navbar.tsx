7caa91e4ba55e1a44f78f2b94b92d21c0b2d37ae
earthGo18 <Earthgo.app@gmail.com>
2025-06-14 14:15:13 +0530

    fixeds2c

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Image from "next/image"
 import { useState, useEffect } from "react"
-import Link from "next/link"
+
 import { Search, Bell } from "lucide-react"
 
 export default function Navbar() {
